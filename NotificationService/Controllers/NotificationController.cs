44f460d6e9c76c7d1058451bc1c2c6907cd9fd50
Konstantin Gazha <46375717+kgazha@users.noreply.github.com>
2022-06-04 22:09:12 +0500

    Add project files.

---
+++ NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,55 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : Controller
+    {
+        [HttpPost]
+        [Route("fire-and-forget")]
+        public IActionResult FireAndForget(string client)
+        {
+            string jobId = BackgroundJob.Enqueue(() =>
+                Console.WriteLine($"{client}, thank you for contacting us."));
+
+            return Ok($"{jobId}");
+        }
+
+        [HttpPost]
+        [Route("delayed")]
+        public IActionResult DelayedJob(string client)
+        {
+            string jobId = BackgroundJob.Schedule(() =>
+                Console.WriteLine($"Session for client {client} has been closed."),
+                TimeSpan.FromSeconds(5));
+
+            return Ok($"{jobId}");
+        }
+
+        [HttpPost]
+        [Route("recurring")]
+        public IActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate(() => 
+                Console.WriteLine("It's high time to work!"), Cron.Daily);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("continuations")]
+        public IActionResult Continuations(string client)
+        {
+            string jobId = BackgroundJob.Enqueue(() =>
+                Console.WriteLine($"Check balance for {client}"));
+
+            BackgroundJob.ContinueJobWith(jobId, () =>
+                Console.WriteLine($"{client}, your balance has been changed"));
+
+            return Ok();
+        }
+    }
+}
